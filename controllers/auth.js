659557b1616654c4c3a54557dc73e1076e423760
danielmamian99 <danimaster99@gmail.com>
2022-12-28 10:55:44 -0500

    encriptar contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const {response} = require('express') // No es necesario, pero sirve para recueprar las ayudas del idle
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const createUser = async(req, res = response) => {// el = response no es necesario, pero ayuda a recuperar las ayudas del idle
@@ -12,6 +13,10 @@ const createUser = async(req, res = response) => {// el = response no es necesar
             });
         }
         user = new User( req.body );
+
+        //encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt );
 
         await user.save();
         
@@ -30,15 +35,44 @@ const createUser = async(req, res = response) => {// el = response no es necesar
 
 };
 
-const userLogin = (req, res) => {
+const userLogin = async(req, res) => {
     const {email, password} = req.body
 
-    res.status(201).json({
-        ok:true,
-        msg:'login',
-        email,
-        password
-    });
+    try {
+
+        let user = await User.findOne({ email });
+
+        if( !user ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Email no valido'
+            }); 
+        }
+
+        //confirmar las contraseñas
+        const validPassword = bcrypt.compareSync( password, user.password );
+
+        if( !validPassword ){
+            return res.status(400).json({
+                ok: false,
+                msg:'Password incorrecto'
+            })
+        }
+        //Generar JWT
+
+        res.status(201).json({
+            ok:true,
+            uid: user.id,
+            name: user.name
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
 }
 
 const renewToken = (req, res) => {
